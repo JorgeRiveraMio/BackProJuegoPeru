d81321f34b87d921f1e48429d690a48fc9174a74
JorgeRiveraMio <jorgeriveramio1008@gmail.com>
2025-04-12 13:25:42 -0500

    primer commit

---
+++ src/test/java/com/projuegoperu/BackProJuegoPeru/BackProJuegoPeruApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projuegoperu.BackProJuegoPeru;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackProJuegoPeruApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
